7397f62a063cabcdc4e3ec9bc2f705114c537c6e
JanetKist <91343154+JanetKist@users.noreply.github.com>
2024-07-02 15:04:14 +0200

    Update get_numeric_bmi.R
    Hi,
    
    Ik heb nog wel wat les in sommige syntax nodig zie ik...
    
    Heb een begin gemaakt van de stukken die ik zo begreep. In CBS zijn er soms leading empty spaces, en ik ga nog even nadenken of we eerst raw files van elke variabele willen, met daarna voorbewerken naar bmi...
    
    ik pas verder aan als ik heb kunnen nadenken hierover
    
    Aan het eind wat reminder syntax voor mezelf toegevoegd.

--- codebook_cbs_R/processing_lab_files/get_numeric_bmi.R
+++ codebook_cbs_R/processing_lab_files/get_numeric_bmi.R
@@ -1,7 +1,7 @@
 #' Function that presents a standardised way to get BMIS, both coded as
 #' BMI and BMI calculated from height and weight
 #'  
-#' For the calculation, the user can give a margin. Measurements of weights
+#' For the calculation, the user can give a margin for weights to be combined with height to get a BMI. Measurements of weights
 #' will be matched with measurements of height if the dBepalingdatum of the
 #' weight is within MARGIN days of the dBepalingdatum of height.
 #' In other words: [dBepalingdatum - margin, dBepalingdatum + margin]
@@ -19,30 +19,31 @@
 #' @param margin of days to scour for matching weight and height measurements
 #' If this is 10, it would look for weights up until 10 days before and
 #' 10 days after the height measurement.
-#' @returns a data table
+#' @returns a data table (see package data.table https://rdatatable.gitlab.io/data.table/)
 #' @example
-#' data <- get_numeric_bmi(data, 10)
+#' data <- get_numeric_bmi(data, 10) (opmerking Janet, ik begrijp niet goed wat dit voorbeeld hier doet, geeft dit de 1e 10 resultaten van bmi?
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("get_numeric_lab_measurements.R")
 library(data.table)
 
+# line 31-86 is 1 funtion, after you run the function, the result is a new file with only bmi
 get_numeric_bmi <- function(data, margin){
 
-  # height is 560
+  # height is 560 #these are dWCIA codes, codes which identify lab or measurements in the Netherlands, more information can be found on NHG lab code viewer: https://bepalingen.nhg.org/labcodes/determinations
   # bmi is c(1234, 1272)
   # weight is c(357, 2408)
   
-  data <- get_numeric_lab_measurements(data, "values", c(560, 1234, 1272, 357, 2408))
+  data <- get_numeric_lab_measurements(data, "values", c(560, 1234, 1272, 357, 2408)) #this selects only the rows with the dWCIAcodes concerning bmi, height and weight
   
-  # bmi
-  bmi <- data[dWCIANummer %in% c(1234, 1272)]
-  bmi[, bmi := values]
+  # bmi #NB: hier is een check nodig, BMI staat af en toe in onlogische waarden of negatieve waarden, zou net als bij height aangepast moeten worden.
+  bmi <- data[dWCIANummer %in% c(1234, 1272)]  # %in% is een call uit base R? optioneel wellicht overwegen (dWCIANummer == “1234”| dWCIANummer == “1272”) als dat sneller is, nog niet getest. 
+  bmi[, bmi := values] #renames column name to bmi
 
   # height
   heightdata <- data[dWCIANummer == 560]
-  heightdata[, height := values]
-  # more clean up
+  heightdata[, height := values] #renames column name to height
+  # some clean up
   heightdata[Eenheid == "m",':=' (height = height * 100) (Eenheid = "cm")]
   heightdata[height < 3, height := height * 100]
   heightdata[height > 10000, height := height / 100]
@@ -83,3 +84,14 @@ get_numeric_bmi <- function(data, margin){
   bmi <- bmi[, c("RINPERSOONS", "RINPERSOON", "dBepalingdatum", "bmi", "bmi_gen"), with = FALSE]
   return(unique(bmi))
 }
+
+#syntax might need to be added
+dt_heigth[ , heigth_gp := str_replace_all(heigth_gp, “,”,”.”] # replace Komma’s by dot’s, Dutch system uses a komma in numericals, R needs dots
+dt_heigth[ , heigth_gp := ifelse(grepl(“\\/+”, heigth_gp), word(gp_heigth$heigth_gp, 1, sep = fixed”/”)), weight] # replaces leading // 
+dt_heigth[ , heigth_gp := gsub(“[^0-9.]”, “”, heigth_gp)] # removes leading empties
+Unique(gp_heigth$heigth_gp) # check what is left in unique results, fix when needed before next step
+dt_heigth[ , heigth_gp := as.numeric(heigth)] # change string to numerical
+dt_heigth = dt_heigth[!is.na(heigth_gp), . (Ids, ID, dBepalingdatum, heigth_gp, Eenheid, dWCIANummer)] # select columns needed for analyses and checks
+dt_heigth = dt_heigth[!duplicated(dt_heigth, by = colnames(dt_heigth), fromLast = TRUE] #remove duplicates
+uniqueN(dt_heigth$ID)
+saveRDS(dt_heigth, file = “filepath/23_gp_heigth.rds”) # save as separate file .rds and .csv are small saving formats
